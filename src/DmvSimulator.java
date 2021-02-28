0453b66e0ffe56a903545923cfa7f463e30ef0be
Heather Kusma <heatherkusma@heathers-air.quinnipiac.edu>
2021-02-28 16:55:38 -0500

    sets up the dmv simulator

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,4 +1,21 @@
+import java.util.Random;
 
 public class DmvSimulator {
+	public static void main (String []args) {
+		System.out.println("Hi! Welcome to the DMV!");
+		Random r = new Random();
+		int min = 1;
+		int max = 100;
+		int randomNum = r.nextInt(max-min)+min;
+		System.out.println("Your number is "+randomNum+ ". Please wait for your number to be called.");
+		for (int i = randomNum+1; i<=max;i++ ) {
+			System.out.println(i);
+		}
+		for (int i = min; i<=randomNum;i++) {
+			System.out.println(i);
+		}
+		System.out.println("Sorry you don't have the required documents. Please come back another day.");
+		
+	}
 
 }
